7f6b2169796d9b59069074e224cd06f8820719c2
manuel pais <manupais22@gmail.com>
2024-09-20 17:00:33 -0300

    Inicializacion de proyecto, creacion de login y menu

---
+++ app/src/main/java/com/example/tp3progavan2/LoginActivity.java
@@ -0,0 +1,31 @@
+package com.example.tp3progavan2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        setSupportActionBar(findViewById(R.id.toolbar));
+        EditText editTextUsername = findViewById(R.id.editTextUsername);
+        EditText editTextPassword = findViewById(R.id.editTextPassword);
+        Button buttonLogin = findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Aquí puedes validar el usuario y la contraseña
+                // Por ahora, simplemente vamos a la MainActivity
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // Termina la actividad de login
+            }
+        });
+    }
+}
